e14c38827f9ead2035e28cf2d28cf611ef2bd8de
chaosdynamix <chaosdynamix@posteo.net>
2022-02-27 03:15:13 +0100

    feat: work on hero and generator responsive rules

--- src/components/generator.svelte
+++ src/components/generator.svelte
@@ -4,9 +4,9 @@
 
 <template>
     <div class="container mx-auto px-6 translate-y-1/2">
-        <div class="w-full h-auto p-6 bg-dark-violet rounded-lg bg-shorten-background bg-no-repeat bg-right-top">
-            <input class="w-full h-12 bg-white rounded-lg px-4 mb-5" placeholder="Shorten a link here..." type="text">
-            <button class="w-full h-12 bg-cyan rounded-lg text-white text-lg">Shorten it!</button>
+        <div class="p-6 bg-dark-violet rounded-lg bg-shorten-background bg-no-repeat bg-right-top md:flex md:flex-row md:gap-4 md:p-10">
+            <input class="w-full h-12 bg-white rounded-lg px-4 mb-5 md:mb-0 md:flex-1" placeholder="Shorten a link here..." type="text">
+            <button class="w-full h-12 bg-cyan rounded-lg text-white text-lg md:w-auto md:px-8 md:flex-shrink-0">Shorten it!</button>
         </div>
     </div>
 </template>
--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-
+    import WorkingIllustration from "@assets/images/illustration-working.svg"
 </script>
 
 <template>
-    <div class="w-full h-auto">
+    <div class="w-full h-auto lg:mt-10 lg:flex lg:flex-row lg:items-center">
 
-        <div class="w-full h-auto pt-[88%] mb-10 bg-working-illustration bg-no-repeat bg-cover bg-[left_1.5rem_top]"></div>
+        <div class="relative w-full h-auto pt-[90%] mb-10 overflow-hidden lg:mb-0 lg:order-2 lg:w-1/2 lg:pt-0 lg:h-[30rem]">
+            <img class="absolute top-0 left-0 w-auto h-full max-w-none ml-6 lg:ml-24" src={WorkingIllustration} alt="Woman working on a computer">
+        </div>
 
-        <section class="container mx-auto px-6 text-center">
-            <h1 class="text-4xl leading-10 text-very-dark-violet font-bold mb-4">More than just shorter links</h1>
-            <p class="text-base leading-6 text-grayish-violet font-medium mb-6">Build your brand's recognition and get detailed insights on how your links are performing.</p>
-            <button class="w-auto h-auto px-10 py-3 bg-cyan rounded-full text-white text-lg font-medium">Get Started</button>
+        <section class="text-center lg:w-1/2 lg:text-left lg:order-1">
+            <div class="w-full h-auto px-6 lg:max-w-[31rem] xl:max-w-[37.5rem] lg:ml-auto">
+                <h1 class="text-4xl leading-10 text-very-dark-violet font-bold mb-4 lg:text-7xl lg:leading-[5rem]">More than just shorter links</h1>
+                <p class="text-base leading-6 text-grayish-violet font-medium mb-6">Build your brand's recognition and get detailed insights on how your links are performing.</p>
+                <button class="w-auto h-auto px-10 py-3 bg-cyan rounded-full text-white text-lg font-medium">Get Started</button>
+            </div>
         </section>
 
     </div>
