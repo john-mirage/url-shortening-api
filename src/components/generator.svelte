905030ae7a60469766d8841ad719a28bbc1a6130
chaosdynamix <chaosdynamix@posteo.net>
2022-02-28 10:22:52 +0100

    feat: add error handling for generator

--- src/components/generator.svelte
+++ src/components/generator.svelte
@@ -10,17 +10,21 @@
 
     let links: Link[] = []
     let textInput
+    let urlIsValid: boolean = true
 
     async function handleSubmit() {
         let schema = string().required().url()
         const url = textInput.value
         if (await schema.isValid(url)) {
+            if (!urlIsValid) urlIsValid = true
             const apiUrl = `https://api.shrtco.de/v2/shorten?url=${url}`
             let data = await fetch(apiUrl).then(response => response.json())
             if (data.ok) {
                 links = [...links, { baseUrl: url, shortUrl: data.result.full_short_link, copied: false }]
                 textInput.value = ""
             }
+        } else {
+            if (urlIsValid) urlIsValid = false
         }
     }
 
@@ -37,14 +41,19 @@
 </script>
 
 <template>
-    <div class="flex flex-col items-stretch gap-5 -translate-y-1/2 p-6 bg-dark-violet rounded-lg bg-shorten-mobile bg-no-repeat bg-right-top md:flex-row md:p-10 lg:bg-shorten-desktop">
+    <div class="relative flex flex-col items-stretch gap-5 -translate-y-1/2 p-6 bg-dark-violet rounded-lg bg-shorten-mobile bg-no-repeat bg-right-top md:flex-row md:p-10 lg:bg-shorten-desktop">
         <input
-            class="w-full h-12 bg-white rounded-lg px-4 md:mb-0 md:flex-1 outline-cyan"
+            class="w-full h-12 bg-white rounded-lg px-4 md:mb-0 md:flex-1 outline-cyan focus:outline-none"
+            class:border-red={ !urlIsValid }
+            class:border-4={ !urlIsValid }
             placeholder="Shorten a link here..."
             type="text"
             bind:this={textInput}
         />
         <FilledButton size="lg" label="Shorten it!" on:click={handleSubmit} />
+        {#if !urlIsValid}
+            <p class="absolute bottom-4 left-10 text-red text-sm font-medium italic">Please add a link</p>
+        {/if}
     </div>
     {#if links.length > 0}
         <ul class="w-full h-auto -translate-y-12 md:-translate-y-16">
